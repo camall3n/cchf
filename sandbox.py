53f71e832907ddf43c63d85f1c7631af853d61af
Cam Allen <csal@brown.edu>
2024-04-12 15:26:03 -0700

    Add gt_utilities, probability calibration; simplify plots

--- sandbox.py
+++ sandbox.py
@@ -52,7 +52,7 @@ def boltzmann_probability(utility1, utility2, temp):
 
 #%%
 # Import MNIST
-N = 100000
+N = 1000000
 (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 (x1_train, x2_train), (u1_train, u2_train) = generate_pref_pairs_dataset(x_train, y_train, N)
 (x1_test, x2_test), (u1_test, u2_test) = generate_pref_pairs_dataset(x_test, y_test)
@@ -61,11 +61,12 @@ N = 100000
 temps = 10**np.random.uniform(low=-2., high=2., size=N).astype(np.float32)
 Tmin = min(temps)
 pr_prefer_1st = boltzmann_probability(u1_train, u2_train, temps)
+pr_prefer_1st_tmin = boltzmann_probability(u1_train, u2_train, Tmin*np.ones_like(temps))
 prefs = np.random.binomial(1, pr_prefer_1st)
-
+prefs_tmin = np.random.binomial(1, pr_prefer_1st_tmin)
 #%%
 sns.histplot(pr_prefer_1st)
-plt.title('Pr(U(x1) > U(x2))')
+plt.title('p_boltz')
 
 #%%
 def build_preference_model():
@@ -82,6 +83,8 @@ def build_preference_model():
         keras.layers.Conv2D(128, kernel_size=(3, 3), activation="relu", kernel_initializer=glorot),
         keras.layers.GlobalAveragePooling2D(),
         keras.layers.Dropout(0.5, seed=seed),
+        # keras.layers.Dense(32, activation="relu", dtype="float32", kernel_initializer=glorot),
+        # keras.layers.Dropout(0.5, seed=seed),
         keras.layers.Dense(1, activation="sigmoid", dtype="float32", kernel_initializer=glorot),
     ])
 
@@ -95,13 +98,17 @@ def build_preference_model():
 
     model = keras.Model(
         inputs=[x1_input, x2_input, temp_input],
-        outputs=pr_prefer_u1
+        outputs=pr_prefer_u1,
     )
-    return model
+    utility_model = keras.Model(
+        inputs=model.input,
+        outputs=[u1, u2],
+    )
+    return model, utility_model
 
 #%% Build a model using the ground-truth temperature data
 true_temp_model = build_preference_model()
-true_temp_model.compile(
+true_temp_model[0].compile(
     loss=keras.losses.BinaryCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=1e-3),
     metrics=[
@@ -109,7 +116,7 @@ true_temp_model.compile(
     ],
 )
 
-true_temp_model.fit(
+true_temp_model[0].fit(
     x=[x1_train, x2_train, temps],
     y=prefs,
     batch_size=32,
@@ -119,14 +126,14 @@ true_temp_model.fit(
 #%% Let's try to build a "perfectly rational" preference model, using a fixed (low) temperature
 fixed_temps = np.ones_like(temps) * temps.min()
 rational_model = build_preference_model()
-rational_model.compile(
+rational_model[0].compile(
     loss=keras.losses.BinaryCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=1e-3),
     metrics=[
         keras.metrics.BinaryAccuracy(name="acc"),
     ],
 )
-rational_model.fit(
+rational_model[0].fit(
     x=[x1_train, x2_train, fixed_temps],
     y=prefs,
     batch_size=32,
@@ -138,14 +145,14 @@ pr_prefer_1st_skyline = boltzmann_probability(u1_train, u2_train, fixed_temps)
 prefs_skyline = np.random.binomial(1, pr_prefer_1st_skyline)
 
 skyline_model = build_preference_model()
-skyline_model.compile(
+skyline_model[0].compile(
     loss=keras.losses.BinaryCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=1e-3),
     metrics=[
         keras.metrics.BinaryAccuracy(name="acc"),
     ],
 )
-skyline_model.fit(
+skyline_model[0].fit(
     x=[x1_train, x2_train, fixed_temps],
     y=prefs_skyline,
     batch_size=32,
@@ -154,7 +161,7 @@ skyline_model.fit(
 
 #%% Build a model using only the low-temperature data
 low_temp_model = build_preference_model()
-low_temp_model.compile(
+low_temp_model[0].compile(
     loss=keras.losses.BinaryCrossentropy(),
     optimizer=keras.optimizers.Adam(learning_rate=1e-3),
     metrics=[
@@ -166,9 +173,27 @@ percentile = 0.1
 temp_cutoff = sorted(temps)[int(N*percentile)]
 low_temp_idx = temps < temp_cutoff
 
-low_temp_model.fit(
+low_temp_model[0].fit(
     x=[x1_train[low_temp_idx], x2_train[low_temp_idx], temps[low_temp_idx]],
     y=prefs[low_temp_idx],
+    batch_size=32,
+    epochs=1,
+)
+
+#%%
+
+gt_utilities_model = build_preference_model()
+gt_utilities_model[1].compile(
+    loss=keras.losses.BinaryCrossentropy(),
+    optimizer=keras.optimizers.Adam(learning_rate=1e-3),
+    metrics=[
+        keras.metrics.MeanSquaredError(name="mse"),
+        keras.metrics.MeanSquaredError(name="mse"),
+    ],
+)
+gt_utilities_model[1].fit(
+    x=[x1_train, x2_train, fixed_temps],
+    y=[u1_train, u2_train],
     batch_size=32,
     epochs=1,
 )
@@ -182,14 +207,14 @@ def kl_divergence(y_true, y_pred):
     vmin[y_pred==0.5] = -np.log(0.5)
     return keras.ops.binary_crossentropy(target=y_true, output=y_pred) - vmin
 
-def evaluate_model(model, model_name, ax=None, type='hist'):
+def plot_model_probs(model, model_name, ax=None, type='hist'):
     pref_classes = {
         '>': u1_test > u2_test,
         '~': u1_test == u2_test,
         '<': u1_test < u2_test,
     }
     fake_temps = Tmin * np.ones(len(x1_test))
-    pr_prefer_left = model.predict((x1_test, x2_test, fake_temps), batch_size=32)
+    pr_prefer_left = model[0].predict((x1_test, x2_test, fake_temps), batch_size=32)
 
     vmin = pr_prefer_left.min()
     vmax = pr_prefer_left.max()
@@ -218,14 +243,81 @@ def evaluate_model(model, model_name, ax=None, type='hist'):
         ax.show()
 
 models = {
-    'skyline_model': skyline_model,
+    # 'skyline_model': skyline_model,
     'rational_model': rational_model,
     'true_temp_model': true_temp_model,
-    'low_temp_model': low_temp_model,
+    # 'low_temp_model': low_temp_model,
+    'gt_utilities_model': gt_utilities_model,
 }
 for type in ['hist', 'kde']:
-    fig, axes = plt.subplots(2,2, figsize=(10, 6), sharex=True)
+    fig, axes = plt.subplots(1,3, figsize=(10, 3), sharex=True)
     for (model_name, model), ax in zip(models.items(), axes.flatten()):
-        evaluate_model(model, model_name, ax=ax, type=type)
+        plot_model_probs(model, model_name, ax=ax, type=type)
     plt.tight_layout()
     plt.show()
+
+
+#%%
+
+models = {
+    # 'skyline_model': skyline_model,
+    'rational_model': rational_model,
+    'true_temp_model': true_temp_model,
+    # 'low_temp_model': low_temp_model,
+    'gt_utilities_model': gt_utilities_model,
+}
+def plot_utility_calibration(model, model_name, ax=None):
+    fake_temps = Tmin * np.ones(len(x1_test))
+    u1_hat, _ = model[1].predict((x1_test, x2_test, fake_temps), batch_size=32)
+
+    should_show_plot = False
+    if ax is None:
+        should_show_plot = True
+        fig, ax = plt.subplots()
+    ax.set_title('Model: ' + model_name)
+    ax.set_xlabel('Actual utility')
+    sns.violinplot(x=9*u1_test, y=9*u1_hat.squeeze(), ax=ax, legend=True)
+    # ax.set_ylim([0,9])
+    ax.set_ylabel('Predicted utility')
+    if should_show_plot:
+        ax.show()
+
+
+fig, axes = plt.subplots(1,3, figsize=(10, 3), sharex=True)
+for (model_name, model), ax in zip(models.items(), axes.flatten()):
+    plot_utility_calibration(model, model_name, ax=ax)
+plt.tight_layout()
+plt.show()
+
+#%%
+
+models = {
+    # 'skyline_model': skyline_model,
+    'rational_model': rational_model,
+    'true_temp_model': true_temp_model,
+    # 'low_temp_model': low_temp_model,
+    'gt_utilities_model': gt_utilities_model,
+}
+def plot_probability_calibration(model, model_name, ax=None):
+    test_temps = 10**np.random.uniform(low=-2., high=2., size=len(x1_test)).astype(np.float32)
+    test_pr_prefer_1st = boltzmann_probability(u1_test, u2_test, test_temps)
+    pr_boltz_hat = model[0].predict((x1_test, x2_test, test_temps), batch_size=32)
+
+    should_show_plot = False
+    if ax is None:
+        should_show_plot = True
+        fig, ax = plt.subplots()
+    ax.set_title('Model: ' + model_name)
+    ax.set_xlabel(r'Actual $p_{boltz}$')
+    sns.kdeplot(x=test_pr_prefer_1st, y=pr_boltz_hat.squeeze(), clip=[0,1], ax=ax, legend=True)
+    ax.set_ylim([0,1])
+    ax.set_ylabel(r'Predicted $\hat p_{boltz}$')
+    if should_show_plot:
+        ax.show()
+
+
+fig, axes = plt.subplots(1,3, figsize=(10, 4), sharex=True)
+for (model_name, model), ax in zip(models.items(), axes.flatten()):
+    plot_probability_calibration(model, model_name, ax=ax)
+plt.tight_layout()
+plt.show()
